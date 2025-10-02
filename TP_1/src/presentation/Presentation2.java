7b5df563bd808c105c656c47dae99e31eaf368ea
Mohamed Amine ELIDRISSI <130193461+Mohamedamine88@users.noreply.github.com>
2025-10-02 10:55:16 +0100

    Add files via upload

---
+++ TP_1/src/presentation/Presentation2.java
@@ -0,0 +1,29 @@
+package presentation;
+import java.util.Scanner;
+import java.io.File;
+import java.lang.reflect.Method;
+
+import dao.IDao;
+import metier.IMetier;
+
+public class Presentation2 {
+	public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(new File("config.txt"));
+        String daoClassName = scanner.nextLine();
+        
+        Class<?> cDao = Class.forName(daoClassName);
+        IDao dao = (IDao) cDao.getDeclaredConstructor().newInstance();
+        
+        String metierClassName = scanner.nextLine();
+        Class<?> cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.getDeclaredConstructor().newInstance();
+        
+        Method setDaoMethod = cMetier.getMethod("setDao", IDao.class);
+        setDaoMethod.invoke(metier, dao);
+        
+        System.out.println("Résultats = " + metier.calcul());
+        
+        scanner.close();
+    }
+
+}
